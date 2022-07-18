2362d75492bd9010f9e6588eaa8d0f885219e506
Ahmad Nizar Nizar <>
2022-07-18 15:41:28 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title('My First Streamlit App')
+st.subheader('Introduction Streamlit in Automate Everything with Python')
+st.write('''This is my first web app enjoy it''')
